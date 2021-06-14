8012b588771243fa657d7ea3d0fe861d2c20b023
talco1 <talco9711@gmail.com>
2021-06-14 18:12:12 +0300

    fixed connection to fg server

--- app/src/main/java/com/example/remotecontroljoystick/model/Model.kt
+++ app/src/main/java/com/example/remotecontroljoystick/model/Model.kt
@@ -1,37 +1,98 @@
 package com.example.remotecontroljoystick.model
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import java.io.PrintWriter
-import java.lang.Thread.sleep
 import java.net.Socket
+import java.util.concurrent.BlockingQueue
+import java.util.concurrent.Executors
+import java.util.concurrent.LinkedBlockingQueue
 
 object Model {
-    private val m_ipAddress = MutableLiveData<String>()
-    val ipAddress: LiveData<String>
-        get() = m_ipAddress
+    private lateinit var out: PrintWriter
+    var dispatchQueue: BlockingQueue<Runnable> = LinkedBlockingQueue<Runnable>()
 
-    private val m_port = MutableLiveData<String>()
-    val port: LiveData<String>
-        get() = m_port
-
-    fun connect() {
+    fun connect(ip: String, port: Int) {
+        //original thread - works
+        /*
         Thread(Runnable {
-            println("BEFORE")
-            val host = m_ipAddress.toString()
-        //    val port = Integer.parseInt(m_port.toString())
-            val fg = Socket("192.168.1.14", 6400)
-            println("AFTER")
-            val out = PrintWriter(fg.getOutputStream(),true)
-            val i=0
-            while(i<10) {
-                out.print("set/controls/flight/rudder -1 \r\n")
+            val fg = Socket(ip, port)
+            out = PrintWriter(fg.getOutputStream(),true)
+            while(true) {
+                out.print("set /controls/flight/rudder -1 \r\n")
                 out.flush()
-                out.print("set/controls/flight/rudder 1 \r\n")
+                out.print("set /controls/flight/rudder 1 \r\n")
                 out.flush()
-                sleep(2)
             }
-            println("END")
         }).start()
+         */
+        //active object
+/*
+        Thread(Runnable {
+            val fg = Socket(ip, port)
+            out = PrintWriter(fg.getOutputStream(),true)
+            while(true) {
+                try {
+                    dispatchQueue.take().run()
+                } catch (e: InterruptedException) {
+                    println("ERROR")
+                }
+            }
+        }).start()
+
+ */
+        //thread pool
+        var executor = Executors.newSingleThreadExecutor();
+        try {
+            executor.submit(Runnable {
+                val fg = Socket(ip, port)
+                out = PrintWriter(fg.getOutputStream())
+                val i=0
+                while(true) {
+                    out.print("set /controls/flight/rudder -1 \r\n")
+                    out.flush()
+                    out.print("set /controls/flight/rudder 1 \r\n")
+                    out.flush()
+                }
+            })
+        } catch (e: InterruptedException) { }
+    }
+
+    fun rudderChanged() {
+        dispatchQueue.put(Runnable {
+            out.print("set /controls/flight/rudder -1 \r\n")
+            out.flush()
+            out.print("set /controls/flight/rudder 1 \r\n")
+            out.flush()
+            out.print("set /controls/flight/aileron -1 \r\n")
+            out.flush()
+            out.print("set /controls/flight/aileron 1 \r\n")
+            out.flush()
+            out.print("set /controls/flight/elevator -1 \r\n")
+            out.flush()
+            out.print("set /controls/flight/elevator 1 \r\n")
+            out.flush()
+            out.print("set /controls/engines/current-engine/throttle 0 \r\n")
+            out.flush()
+            out.print("set /controls/engines/current-engine/throttle 1 \r\n")
+            out.flush()
+        })
     }
 }
+
+/*
+                out.print("set /controls/flight/rudder -1 \r\n")
+                out.flush()
+                out.print("set /controls/flight/rudder 1 \r\n")
+                out.flush()
+                out.print("set /controls/flight/aileron -1 \r\n")
+                out.flush()
+                out.print("set /controls/flight/aileron 1 \r\n")
+                out.flush()
+                out.print("set /controls/flight/elevator -1 \r\n")
+                out.flush()
+                out.print("set /controls/flight/elevator 1 \r\n")
+                out.flush()
+                out.print("set /controls/engines/current-engine/throttle 0 \r\n")
+                out.flush()
+                out.print("set /controls/engines/current-engine/throttle 1 \r\n")
+                out.flush()
+ */
--- app/src/main/java/com/example/remotecontroljoystick/view_model/MainViewModel.kt
+++ app/src/main/java/com/example/remotecontroljoystick/view_model/MainViewModel.kt
@@ -1,15 +1,26 @@
 package com.example.remotecontroljoystick.view_model
 
 import androidx.databinding.Bindable
+import androidx.databinding.Observable
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.remotecontroljoystick.model.Model
 
-class MainViewModel : ViewModel() {
-    val ipAddress: LiveData<String>
-        get() = Model.ipAddress
-    val port: LiveData<String>
-        get() = Model.port
-    fun connectFlightGear() = Model.connect()
+class MainViewModel : ViewModel(), Observable {
+
+    @Bindable
+    val ipAddress = MutableLiveData<String>()
+    @Bindable
+    val port = MutableLiveData<String>()
+
+    override fun addOnPropertyChangedCallback(callback: Observable.OnPropertyChangedCallback?) {
+    }
+
+    override fun removeOnPropertyChangedCallback(callback: Observable.OnPropertyChangedCallback?) {
+    }
+
+    fun connectFlightGear() = Model.connect(ipAddress.value.toString(), (port.value.toString()).toInt())
+
+    fun rudder() = Model.rudderChanged()
 }
